4c9da5b3a6c489704253a27f7c7530d800312836
alfisalvacion <jmsalvacion1@up.edu.ph>
2017-05-03 22:43:35 +0800

    fixed minor mistakes where the regex that is accepted in signup and login doesn't allow a / in the end

--- listr/urls.py
+++ listr/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name=''),
-    url(r'^login$', views.login, name=''),
-    url(r'^signup$', views.signup, name=''),
-    url(r'^questions/', include('questions.urls'))
+    url(r'^login/', views.login, name=''),
+    url(r'^signup/', views.signup, name=''),
+    url(r'^questions/', include('questions.urls')),
 ]
