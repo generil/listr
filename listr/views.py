77a24abacb2be3cd4e125decbd012ac52f3d9c61
Generil de los Santos <generild@gmail.com>
2017-05-04 23:04:35 +0800

    updated

--- listr/views.py
+++ listr/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-	return HttpResponse("<h1> Welcome to Listr.io </h1>")
+	# return HttpResponse("<h1> Welcome to Listr.io </h1>"
+	return render(request, "index.html", context=None)
 
 def login(request):
 	return HttpResponse("<h1> Login to Listr.io </h1>")
