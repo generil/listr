3d9318559647d3e5ad081ef67c832b56cc51a02a
Generil de los Santos <generild@gmail.com>
2017-05-05 23:43:43 +0800

    renamed user to questions

--- listr/urls.py
+++ listr/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('user.urls')),
+    url(r'^$', include('questions.urls')),
     url(r'^admin/', admin.site.urls),
 ]
