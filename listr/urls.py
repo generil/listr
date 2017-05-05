623e1cd56b176d2828d62db83b5b674d8f14ceb0
Generil de los Santos <generild@gmail.com>
2017-05-05 15:34:31 +0800

    html works!!!

--- listr/urls.py
+++ listr/urls.py
@@ -1,11 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from . import views
 
 urlpatterns = [
+    url(r'^$', include('user.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index, name=''),
-    url(r'^login/', views.login, name=''),
-    url(r'^signup/', views.signup, name=''),
-    url(r'^questions/', include('questions.urls')),
 ]
