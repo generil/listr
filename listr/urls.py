e81e66b8486f1cdbda84c1764fc6f1eb5a5f4202
alfisalvacion <jmsalvacion1@up.edu.ph>
2017-05-06 01:27:43 +0800

    added models and migrated

--- listr/urls.py
+++ listr/urls.py
@@ -2,10 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-<<<<<<< HEAD
-    url(r'^', include('user.urls')),
-=======
-    url(r'^$', include('questions.urls')),
->>>>>>> 315d1413d97b266642d5b619070fe217511c04e1
+    url(r'^', include('questions.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- questions/apps.py
+++ questions/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class UserConfig(AppConfig):
-    name = 'user'
+class QuestionsConfig(AppConfig):
+    name = 'questions'
