37904cd8078affcc3be11538fc255d1a6b449830
alfisalvacion <jmsalvacion1@up.edu.ph>
2017-05-06 13:40:42 +0800

    finalized models

--- questions/models.py
+++ questions/models.py
@@ -11,8 +11,8 @@ class User(models.Model):
 	join_date = models.DateField()
 	is_verified = models.BooleanField(default = True)
 
-	def full_name(self):
-		return first_name + ' ' + last_name
+	def __str__(self):
+		return self.first_name + ' ' + self.last_name
 
 class Topic(models.Model):
 	topic = models.CharField(max_length = 30, unique = True)
@@ -20,26 +20,37 @@ class Topic(models.Model):
 	user_id = models.ForeignKey(User, on_delete = models.CASCADE)
 	create_date = models.DateField()
 
-class Answer(models.Model):
-	answer = models.CharField(max_length = 500)
-	# question = models.ForeignKey(Question, primary_key = True, on_delete = models.CASCADE)
-	user_id = models.ForeignKey(User, on_delete = models.CASCADE)
-	answer_date = models.DateField()
-	upvotes = models.IntegerField()
-	downvotes = models.IntegerField()
+	def __str__(self):
+		return self.topic
 
 class Question(models.Model):
-	question = models.CharField(max_length = 30, unique = True)
+	question = models.CharField(max_length = 30)
 	details = models.CharField(max_length = 100)
 	user_id = models.ForeignKey(User, on_delete = models.CASCADE)
 	question_date = models.DateField()
 	upvotes = models.IntegerField()
 	downvotes = models.IntegerField()
 	topic = models.ForeignKey(Topic, on_delete = models.CASCADE)
-	answers = models.ManyToManyField(Answer)
+
+	def __str__(self):
+		return self.question
+
+class Answer(models.Model):
+	answer = models.CharField(max_length = 500)
+	question = models.ForeignKey(Question, on_delete = models.CASCADE)
+	user_id = models.ForeignKey(User, on_delete = models.CASCADE)
+	answer_date = models.DateField()
+	upvotes = models.IntegerField()
+	downvotes = models.IntegerField()
+
+	def __str__(self):
+		return self.answer
 
 class Comment(models.Model):
 	comment = models.CharField(max_length = 50)
 	answer = models.ForeignKey(Answer, on_delete = models.CASCADE)
 	user_id = models.ForeignKey(User, on_delete = models.CASCADE)
 	comment_date = models.DateField()
+
+	def __str__(self):
+		return self.comment
