5b972dfaf3f29bf8bee31735a96d889302c1af84
alfisalvacion <jmsalvacion1@up.edu.ph>
2017-05-06 14:10:41 +0800

    experimented on templates and urls

--- questions/views.py
+++ questions/views.py
@@ -2,4 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'login.html')
+  return render(request, 'login.html')
+
+def login(request):
+	return HttpResponse("<h1>Logged in!</h1>")
+
+def signup(request):
+	return HttpResponse("")
