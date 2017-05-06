c72054c0b68523cc4bb16444fc657850127437ee
alfisalvacion <jmsalvacion1@up.edu.ph>
2017-05-06 22:51:01 +0800

    added views and urls for list of questions

--- questions/urls.py
+++ questions/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	# /signup/
   url(r'^$', views.index, name='index'),
+  # /questions/
+  url(r'^questions/$', views.questions, name='questions'),
+  # /qustions/id
+  url(r'^questions/(?P<question_id>[0-9]+)/$', views.question_detail, name='question_detail'),
 ]
