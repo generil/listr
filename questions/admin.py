7bafd887536e9051d8b7688602f4450e0933a560
Generil de los Santos <generild@gmail.com>
2017-05-07 16:28:46 +0800

    bug fixes

--- questions/admin.py
+++ questions/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 # from models import User
-from models import Topic
-from models import Question
-from models import Answer
-from models import Comment
+from .models import Topic
+from .models import Question
+from .models import Answer
+from .models import Comment
 
 admin.site.register(Topic)
 admin.site.register(Question)
