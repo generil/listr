60e28dacc4fb6cbd6230dc4872563145a95bd791
Generil de los Santos <generild@gmail.com>
2017-05-10 21:36:35 +0800

    refactoring code

--- questions/views.py
+++ questions/views.py
@@ -1,5 +1,4 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
