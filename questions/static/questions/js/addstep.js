eeefddba77d302b4b7c32aab5a318cf31a67f022
Generil de los Santos <generild@gmail.com>
2017-05-16 17:25:30 +0800

    fixed some bugs and shit

--- questions/static/questions/js/addstep.js
+++ questions/static/questions/js/addstep.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   $(".add-step").click(function(){
+    // add step
     $(".step-list").append('<li><textarea class="form-control" rows="1" style="margin-top:10px;"></textarea></li>');
-    x = $(".step-list li").length + " STEPS";
+    var x = $(".step-list li").length + " STEPS";
     document.getElementById('step-indicator').innerText=x;
   });
 });
