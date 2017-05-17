9aad72719c47cd84f0a6185ee9a8135ecd95f456
Generil de los Santos <generild@gmail.com>
2017-05-17 20:12:33 +0800

    fixed some nasty bugs

--- questions/static/questions/js/addstep.js
+++ questions/static/questions/js/addstep.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".add-step").click(function(){
-    // add step
+    // add step 
     $(".step-list").append('<li><textarea class="form-control" rows="1" style="margin-top:10px;"></textarea></li>');
     var x = $(".step-list li").length + " STEPS";
     document.getElementById('step-indicator').innerText=x;
